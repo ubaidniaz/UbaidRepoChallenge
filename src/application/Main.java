f7b8aef0af6170d0aad3bc6e789326c2a9ecb636
mohammad.soomro <mohammad.soomro@IC-CS233-27.uc.ucalgary.ca>
2022-07-22 20:51:10 +0300

    added a label and a branch to teammates repo in my branch

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Mohammad's Label"));
+			root.getChildren().add(new Label("Mohammad's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ubaid's Project");
